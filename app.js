0f30b0a87fff42aa94efd7f2db7fab21710df1a1
Vamsee <vamsee@m.com>
2017-04-13 17:31:28 -0400

    Updated bot to speak

--- app.js
+++ app.js
@@ -32,7 +32,43 @@ app.get('/webhook/', function (req, res) {
 	res.send('Error, wrong token')
 })
 
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+	    let event = req.body.entry[0].messaging[i]
+	    let sender = event.sender.id
+	    if (event.message && event.message.text) {
+		    let text = event.message.text
+		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+	    }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAAFZBGOx4hNEBAAVTRBci8Q7fYHudZCbmZC86zzjuB6yYLnz5qVFZCGd3tvBc50EfYZAZAttKuzVAzXl3DhCrZB1NZAGO6nnbfX2SIPJq7oETdM0SGXZA86PXzS6j3bWoDFSrmtuAXLiEJJiJfcamfqoPWtfFs77cx4riDXLa6eJ1WgZDZD"
+
+function sendTextMessage(sender, text) {
+	let messageData = { text:text }
+	
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+			recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error)
+		}
+	})
+}
+
 // Spin up the server
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
+
