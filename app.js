1ef5e5a7e4d0e3137056e19a81ec38afba3502f9
Vamsee <vamsee@m.com>
2017-04-24 23:13:55 -0400

    quick replies feature

--- app.js
+++ app.js
@@ -71,24 +71,25 @@ function sendTextMessage (sender, text) {
 function sendTextMessageQR (sender, information) {
   //check if sender is in database or not
   let textData = 'You are currently not in a group. Please select an option.'
-  let quickRepliesData = [
-  {
-    content_type: 'text',
-    title: 'Join existing group'
-    payload: 'join_group'
-  },
-  {
-    content_type: 'text',
-    title: 'Create new group'
-    payload: 'new_group'
-  }
-  ]
+  //let quickRepliesData = 
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
     qs: {access_token:token},
     json: {
       recipient: {id: sender},
-      message: {text: textData, quick_replies: quickRepliesData},
+      message: {text: textData, 
+        quick_replies: [
+        {
+          content_type: 'text',
+          title: 'Join existing group'
+          payload: 'join_group'
+        },
+        {
+          content_type: 'text',
+          title: 'Create new group'
+          payload: 'new_group'
+        }]
+      },
     }
   }, function (error, response, body) {
     if (error) {
