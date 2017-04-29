84bd43e3ad84ad5aea958879d3b8bba433442330
Vamsee <vamsee@m.com>
2017-04-28 20:33:01 -0400

    lean

--- app.js
+++ app.js
@@ -69,6 +69,8 @@ app.post('/webhook/', function (req, res) {
 
                 console.log('group is ' + g)
                 console.log('g1 is ' + g1)
+                console.log('g1 stringify is ' + JSON.stringify(g1))
+                console.log('g1[0] ' + g1[0])
                 //console.log(g1.name + ' ' + g1.roommmates)
                 //console.log('to String is ' + g.toString())
                 //console.log('name is ' + g.name + ' roommmates are ' + g.roommmates)
