581964fbbe6d5aa4fa13b07452c0f8fcf2561022
Vamsee <vamsee@m.com>
2017-04-28 23:00:01 -0400

    fixing remove user

--- database.js
+++ database.js
@@ -84,7 +84,7 @@ groupSchema.statics.checkPassword = function (groupName, password, cb) {
 }
 
 groupSchema.statics.removeUser = function (userId, cb) {
-  this.update({roommates: {$elemMatch: {id: userId}}}, {$pull: {roommates: {$in : userId}}}, function (error) {
+  this.update({roommates: {$elemMatch: {id: userId}}}, {$pull: {roommates: {$elemMatch : userId}}}, function (error) {
     if (error) {
       cb(error)
     } else {
